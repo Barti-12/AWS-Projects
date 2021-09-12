9d1078a1f259e6edcb65b4cf2e4b57042f451cf0
barti12 <60892908+Barti-12@users.noreply.github.com>
2021-09-12 13:32:42 +0200

    Create Lambda Json to S3.py

---
+++ Lambda scripts/Lambda Json to S3.py
@@ -0,0 +1,18 @@
+import json
+import boto3
+
+s3=boto3.client('s3')
+
+def lambda_handler(event, context):
+    bucket='somebucket'
+    
+    somejson= {"name":"Bartek", "age":24, "food":"kebab, pizza","free":True}
+    
+    fileName='testjs' + '.json'
+    
+    uploadFile=bytes(json.dumps(somejson).encode('UTF-8'))
+    
+    s3.put_object(Bucket=bucket,Key=fileName,Body=uploadFile)
+    
+    print("Status Ok")
+
